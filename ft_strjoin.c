b4422efedfceca194b4dc817efd9008aaf90c744
Clantpax <pybbpybb@gmail.com>
2021-08-17 02:58:30 +0800

    SOLI

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -19,27 +19,23 @@ char	*ft_strjoin(const char *s1, const char *s2)
 	size_t	j;
 	size_t	k;
 
-	k = 0;
+	if (!s1 || !s2)
+		return (0);
+	k = -1;
 	i = ft_strlen(s1);
 	j = ft_strlen(s2);
 	a = (char *)malloc(sizeof(char) * (i + j) + 1);
-	if (!a || !s1 || !s2)
+	if (!a)
 		return (0);
-	while (k < i)
-	{
+	while (++k < i)
 		a[k] = s1[k];
-		k++;
-	}
-	k = 0;
-	while (k < i + j)
-	{
+	k = -1;
+	while (++k < i + j)
 		a[k + i] = s2[k];
-		k++;
-	}
 	a[i + j] = 0;
 	return (a);
 }
-
+/*
 int main()
 {
     char *a = "big";
@@ -47,3 +43,4 @@ int main()
 
     printf("%s\n", ft_strjoin(a, b));
 }
+*/
