02d9c8551dc5f8d06cc12ef8bc39074500560294
Clantpax <pybbpybb@gmail.com>
2021-08-17 21:27:28 +0800

    TELLMEWHY

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -35,7 +35,7 @@ void	ft_putnbr_fd(int n, int fd)
 		write(fd, "-", 1);
 		ft_putnbr_fd(n * -1, fd);
 	}
-	else if (n > 10)
+	else if (n > 9)
 	{
 		ft_putnbr_fd(n / 10, fd);
 		ft_putnbr_fd(n % 10, fd);
