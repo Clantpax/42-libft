37ae88d0570e0a139eac0401f4d72a11b8132c2f
Clantpax <pybbpybb@gmail.com>
2021-08-17 17:07:38 +0800

    last tester

--- ft_strchr.c
+++ ft_strchr.c
@@ -19,11 +19,11 @@ char	*ft_strchr(const char *s, int c)
 	i = 0;
 	while (*(s + i) != 0)
 	{
-		if (*(s + i) == (char *)c)
+		if (*(s + i) == (char)c)
 			return ((char *)s + i);
 		i++;
 	}
-	if (*(s + i) == (char *)c)
+	if (*(s + i) == (char)c)
 		return ((char *)s + i);
 	return (0);
 }
