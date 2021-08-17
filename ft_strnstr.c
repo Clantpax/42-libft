45d16fc00652875462253c5abf85b967f1414163
Clantpax <pybbpybb@gmail.com>
2021-08-17 15:08:59 +0800

    a

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -16,7 +16,9 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	size_t	i;
 	size_t	j;
-	if (!big || !little || len == 0)
+	if (!big || len == 0)
+		return (0);
+	if (!little)
 		return (ft_strdup(""));
 	i = 0;
 	while (big[i] != 0 && i < len)
