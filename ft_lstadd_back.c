7b52902e09b9c5b21aaf603f3837c4b7bc22ca70
Clantpax <pybbpybb@gmail.com>
2021-08-19 03:22:50 +0800

    w

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -16,7 +16,7 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*a;
 
-	if (!lst)
+	if (!(*lst))
 	{
 		(*lst) = new;
 		return ;
@@ -24,22 +24,22 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 	a = ft_lstlast(*lst);
 	a->next = new;
 }
-/*
+/* 
 int main()
 {
-	char *str = "testr";
+	//char *str = "testr";
 	char *str2 = "aaaaa";
-	t_list *lst = ft_lstnew(str);
+	t_list *lst = NULL;
 	t_list *lst2 = ft_lstnew(str2);
-	printf("%s\n", (char *)lst->content);
-	printf("%s\n\n", (char *)lst2->content);
+	//printf("%s\n", (char *)lst->content);
+	printf("%s\n", (char *)lst2->content);
 	ft_lstadd_back(&lst, lst2);
 	printf("%s\n", (char *)lst->content);
 	lst = lst->next;
-	printf("%s\n\n", (char *)lst->content);
+	//printf("%s\n\n", (char *)lst->content);
 	ft_lstadd_back(&lst, lst2);
 	printf("%s\n", (char *)lst->content);
 	lst = lst->next;
 	printf("%s\n", (char *)lst->content);
 }
-*/
+ */
