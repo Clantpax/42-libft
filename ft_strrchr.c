a14b9327b3791f9bc11279383f0cc9b71d0ffecc
Clantpax <pybbpybb@gmail.com>
2021-08-20 17:11:16 +0800

    sed

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -14,15 +14,13 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	size_t	i;
-	size_t	j;
+	int	i;
 
 	i = ft_strlen(s);
-	j = ft_strlen(s);
 	if (*(s + i) == (unsigned char)c)
 		return ((char *)s + i);
 	i--;
-	while (*(s + i) != 0 && (int)i > -1)
+	while (*(s + i) != 0 && i > -1)
 	{
 		if (*(s + i) == (unsigned char)c)
 			return ((char *)s + i);
