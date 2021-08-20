8c0a4ba39c2e927499a44c9c97b97bc684942f70
Clantpax <pybbpybb@gmail.com>
2021-08-20 17:29:55 +0800

    null

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -18,11 +18,11 @@ size_t	ft_strlcat(char *dest, const char *src, size_t size)
 	size_t	j;
 	size_t	k;
 
+	if (!src)
+		return (0);
 	j = ft_strlen(dest);
 	k = ft_strlen(src);
 	i = 0;
-	if (size == 0)
-		return (k);
 	while (j + i < size - 1 && *src != 0)
 		dest[j + i++] = *src++;
 	dest[j + i] = 0;
